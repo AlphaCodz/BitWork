49aa06b361599466c5693584b78e6ed539cc9a3e
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-06-09 16:40:35 +0100

    Prod Ready

--- project/project/settings.py
+++ project/project/settings.py
@@ -42,9 +42,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bit"
+
 ]
 
+# RAILWAY STORAGE
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
